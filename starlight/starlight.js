f75a359c0e5f3a20ee1090ff5b06a550ad2ce51c
Serge Babayan <serj.b.babayan@gmail.com>
2015-05-01 18:14:13 -0400

    Added basic user configuration

---
+++ starlight/starlight.js
@@ -0,0 +1,15 @@
+var user_configuration={
+	shape:"circle", //could also be square
+	final_size:"24px",
+	initial_size:"12px",
+	expand_speed:"1s",
+	colors:["red"],
+	frequency:1, //how often a new wave of stars popout
+	density: 1, //how many stars pop out per wave
+	keep_lit: false, //whether the stars dissapear after they are created
+	rotation: false //whether the stars rotate through out their expansion
+	coverage:100 //how much of the element (in percent) the stars will show up in
+};
+$(document).ready(function(){
+
+});
